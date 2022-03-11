cb2558544fc356e7690f7364647195e22855032a
elystanz <elysestanziale@gmail.com>
2022-03-11 14:09:45 -0600

    in the process of making list elements sortable and save updates to localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -75,8 +75,8 @@ $(".list-group").on("blur", "textarea", function() {
   .closest(".list-group-item")
   .index();
 
-  tasks[status][index].text = text;
-  saveTasks();
+  // tasks[status][index].text = text;
+  // saveTasks();
 
   // recreate p element
   var taskP = $("<p>")
@@ -126,8 +126,8 @@ $(".list-group").on("blur", "input[type='text']", function() {
   .index();
 
   // update task in array and re-save to localStorage
-  tasks[status][index].date = date;
-  saveTasks();
+  // tasks[status][index].date = date;
+  // saveTasks();
 
   // recreate span element with bootstrap classes
   var taskSpan = $("<span>")
@@ -169,6 +169,48 @@ $("#task-form-modal .btn-primary").click(function() {
     });
 
     saveTasks();
+  }
+});
+
+// make list elements sortable
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event) {
+    console.log("activate", this);
+  },
+  deactivate: function(event) {
+    console.log("deactivate", this);
+  },
+  over: function(event) {
+    console.log("over", event.target);
+  },
+  out: function(event) {
+    console.log("out", event.target);
+  },
+  update: function(event) {
+    var tempArr = [];
+    // loop over current set of children in sortable list
+    $(this).children().each(function() {
+      var text = $(this)
+      .find("p")
+      .text()
+      .trim();
+
+      var date = $(this)
+      .find ("span")
+      .text()
+      .trim();
+
+      // add task data to the temp array as an object
+      tempArr.push({
+        text: text,
+        date: date
+      });
+    });
+    console.log(tempArr);
   }
 });
 
